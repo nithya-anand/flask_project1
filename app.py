e6c22767eaee1b33ea8a2cabcf9611249068125b
nithya-schome <nithyon1793@gmail.com>
2021-05-18 20:03:31 +0530

    thankyou file added and file updated with submit button

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request
 
 app=Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('form.html')
-    
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    a=request.form.get('exp')
+    b=request.form.get('name')
+    c=request.form.get('mail')
+    d=request.form.get('phone_number')
+    print(a,b,c,d)
+
+    return render_template('thank.html')
+
 app.run(debug=True)
